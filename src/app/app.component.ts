bc666be08cf01eabf23b51d5ce1c9fd7216c84fe
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-12-28 05:31:03 -0800

    Finished project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,24 @@ import { lorem, random } from 'faker';
 })
 export class AppComponent {
   title = 'angular-typing-game';
-  randomText = lorem.sentence()
-  inputTextMatch = false
+  randomText = lorem.sentence();
+  inputTextMatch = false;
+  enteredText = '';
 
   onTyping(value: string) {
     if (this.randomText === value) {
       this.inputTextMatch = true
     }
+
+    this.enteredText = value;
+  }
+
+  compare(correctLetter:string, enteredText:string) {
+    if (!enteredText) {
+      return 'pending';
+    }
+    //if matching letters, return a string of 'correct', otherwise, return 'incorrect'
+    //then style based on these returned classses
+    return correctLetter === enteredText ? 'correct' : 'incorrect'
   }
 }
